c9fef60293e40bb5a17467398e947b5c026db549
Patrick McCombs <pa.mccombs1@gmail.com>
2018-10-19 13:10:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ function nestedTarget(){
   return document.querySelector(`#nested .target`)
 }
 
-function deepestChild(){
-  let n = document.getElementById('grand-node')
+function increaseRankBy(n) {
+ const lis = document.querySelectorAll('ul.ranked-list li');
+ for (var i=0; i < lis.length; i++) {
+   lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n).toString();
+ }
 }
+ function deepestChild() {
+  var elements = document.querySelector('#grand-node div');
+  var nextElement = elements.children[0];
+  while (nextElement) {
+    elements = nextElement;
+    nextElement = elements.children[0];
+  }
+  return elements;
+ }
